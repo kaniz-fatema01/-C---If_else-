71d41179c3d2deb69ebbe7c814b96f981219a490
Kaniz Fatema <78794903+kaniz-fatema01@users.noreply.github.com>
2021-02-13 21:08:08 +0600

    Create Eligible for vote.cpp

---
+++ Eligible for vote.cpp
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main()
+{
+    float a,b;
+    printf("a= ");
+    scanf("%f", &a);
+    
+    
+if (a<18)
+{  
+    printf("Not Eligible");
+}
+else
+{
+    
+    printf("Eligible");
+}
+
+    return 0;
+}
