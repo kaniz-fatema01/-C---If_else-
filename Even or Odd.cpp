f1f8b974247254ac8f3db8ee59602d579245387d
Kaniz Fatema <78794903+kaniz-fatema01@users.noreply.github.com>
2021-02-13 20:59:41 +0600

    Create Even or Odd.cpp

---
+++ Even or Odd.cpp
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main()
+{
+    int a;
+    printf("a= ");
+    scanf("%d", &a);
+    
+if (a%2 ==1)
+{  
+    printf("Odd");
+}
+else
+{
+    
+    printf("Even");
+}
+
+    return 0;
+}
